b18fcdabef1b7b7377cafd371dfcc81a4d6cbaa2
Top Tester <json900064@gmail.com>
2018-12-21 14:23:09 -0500

    Trying to fix gon initializer error with db migrate

--- config/initializers/gon_initalizer.rb
+++ config/initializers/gon_initalizer.rb
@@ -1,2 +1,4 @@
 #Use global Gon var for now until come up with something better
-Gon.global.artists = Artist.all.order(:name).map { |a| [a.name,a.id] }
+if Artist.exists?
+  Gon.global.artists = Artist.all.order(:name).map { |a| [a.name,a.id] }
+end
